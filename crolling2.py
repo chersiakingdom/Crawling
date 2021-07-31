f6453bef8befd122a4b760ea9a6b23231b07b4c7
Daye Kim <rlaek4793@khu.ac.kr>
2021-08-01 02:08:57 +0900

    Delete crolling2.py

--- crolling2.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-link = 'https://www.linkareer.com/list/activity'
-raw = requests.get(link)
-sour = BeautifulSoup(raw.text, "html.parser")
-print(sour)
